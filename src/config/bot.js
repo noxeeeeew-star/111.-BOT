82e408b25f59da62fccad1d7f1fcc0e707aef2aa
noxeeeeew-star <noxeeeeew@gmail.com>
2026-08-21 17:36:28 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "111", // required by Discord API, not shown in the client
-        state: "test",     // this is what people actually see
+        name: "NOXEEEEE BOT", // required by Discord API, not shown in the client
+        state: "PLAYING VALO",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
