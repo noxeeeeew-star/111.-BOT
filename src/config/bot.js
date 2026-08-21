484c20ab3c66cc739bce9026952ac7829d127d92
noxeeeeew-star <noxeeeeew@gmail.com>
2026-08-21 17:28:55 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "111", // required by Discord API, not shown in the client
-        state: "4",     // this is what people actually see
+        state: "test",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
