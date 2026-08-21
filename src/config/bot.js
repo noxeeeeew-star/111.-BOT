e5d82473ca78f69be58a5faba633d89358a21128
noxeeeeew-star <noxeeeeew@gmail.com>
2026-08-21 17:27:24 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        name: "111", // required by Discord API, not shown in the client
+        state: "4",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
